f5550fb37b259542a90e71cc3148ebc9295fea5f
Bharath Chandra <muppasanibharath.18@gmail.com>
2025-12-26 18:33:13 -0500

    update

--- plugin.js
+++ plugin.js
@@ -1,5 +1,5 @@
 // --- General Configs ---
-const PY_MODULE_URL = "https://rawcdn.githack.com/ai4society/planning-ontology-tool/bc820971d8aa0c28412d65721f5726c028c74701/ontology.py";
+const PY_MODULE_URL = "https://rawcdn.githack.com/ai4society/planning-ontology-tool/be061b1f0b57621a8320133f07c837b35de3c5fe/ontology.py";
 const PY_MODULE_NAME = "ontology";
 const PY_FUNC_NAME = "create_ontology";
 
